60430af6421ab8aaec69290a1100833def996d3d
archanameena <169147657+archanameena9630@users.noreply.github.com>
2024-08-23 22:40:53 +0530

    Add files via upload

---
+++ String_replece.java
@@ -0,0 +1,11 @@
+package string;
+// Replace "a" with "o" in the string "banana".
+public class String_replece {
+    public static void main(String[] args) {
+        String str="hello world";
+        String str1=str.replace("world","java");
+        System.out.println("orijanle:"+str);
+        System.out.println("modifaiyr:"+str1);
+
+    }
+}
