81a44242037cc75d650c4efd97be038853ef6f46
archanameena <169147657+archanameena9630@users.noreply.github.com>
2024-08-23 22:38:51 +0530

    Add files via upload

---
+++ assigment4.java
@@ -0,0 +1,9 @@
+package string;
+
+ class assigment4 {
+    public static void main(String[] args) {
+    String str="PHP exercises and";
+    str=str+" python exercies";
+    System.out.println(str);
+}
+}
