721e10334af983a07c0b18681129b7cc09b75d70
archanameena <169147657+archanameena9630@users.noreply.github.com>
2024-08-23 22:38:23 +0530

    Add files via upload

---
+++ assigment3.java
@@ -0,0 +1,61 @@
+package string;
+
+public class assigment3 {
+    public static void main(String[] args) {
+        String s1=" this is exerice 1";
+        String s2=" this is Exerice 1";
+        if(s1!=s2){
+            System.out.println(" this is not equal");
+        }
+        else {
+            System.out.println(" this is equal");
+        }
+        }
+    }
+    // ==============================================================
+//     String s1=" this is exerice 1";
+//         String s2=" this is Exerice 1";
+    
+//          int num1=0;
+//         int num2=0;
+//         for(int i=0;i<s1.length();i++){
+//             num1=(int)s1.charAt(i);
+//             num2=(int)s2.charAt(i);
+//         }
+//         if(num1>num2){
+//             System.out.print(" this is not equal");
+//         }
+//         else {
+//             System.out.print(" this is equal");
+//         }
+//     }
+// }
+// =========================================================
+// if(s1.length()==s2.length()){
+//     for(int i=0;i<s1.length();i++){
+//        if(s1.charAt(i)==s2.charAt(i)){
+//            count++;
+//        }
+//        int ch=s1.charAt(i);
+//        int ch1=s2.charAt(i);
+
+//         if(ch+32==ch1){
+//            count++;
+//         }
+//         if(ch-32==ch1){
+//            count++;
+//         }
+//     }
+//    }
+//    else{
+//        System.out.println("this is not same");
+//    }
+//    if(count==s1.length()&&count==s2.length()){
+//        System.out.println(" thi is same");
+//    }
+//    else{
+//        System.out.println(" cheracter is not same");
+//    }
+// }
+
+// }
