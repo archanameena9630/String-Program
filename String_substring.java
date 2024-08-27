d3309e81733b7bbb4186bd976a2429a39be6d646
archanameena <169147657+archanameena9630@users.noreply.github.com>
2024-08-27 22:13:44 +0530

    Add files via upload

---
+++ String_substring.java
@@ -0,0 +1,9 @@
+package string;
+// Extract the substring "GPT" from "ChatGPT"
+public class String_substring {
+    public static void main(String[] args) {
+        String str="chatGPT";
+        String str1=str.substring(4);
+        System.out.println(str1);
+    }
+}
