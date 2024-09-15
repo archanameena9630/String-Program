b773f8b6413eea186c1da0e40a6288441d2bd521
archanameena <169147657+archanameena9630@users.noreply.github.com>
2024-09-15 23:31:13 +0530

    Add files via upload

---
+++ index_string_.java
@@ -0,0 +1,12 @@
+package string;
+
+     class String4 {
+        public static void main(String[] args) {
+            String  a="goodmorning";
+            char ch=a.charAt(5);
+            System.out.println(a.charAt(4));
+            System.out.println(ch);
+            // System.out.println(a.indent(ch));
+        }
+    }
+
