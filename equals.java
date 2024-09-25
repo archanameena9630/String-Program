019d651d73fb15e730637654505601acbbb800b7
archanameena <169147657+archanameena9630@users.noreply.github.com>
2024-09-25 23:22:35 +0530

    Add files via upload

---
+++ equals.java
@@ -0,0 +1,14 @@
+package string;
+
+public class equals {
+    public static void main(String[] args) {
+        String s1="stephen edwin king";
+        String s2="walter winchell";
+        String s3="mike royko";
+
+System.out.println("\""+s1+"\""+" equel"+"\""+s2+"\""+"?="+s1.equals(s2));
+System.out.println("\""+s1+"\""+"equel"+"\""+s3+"\""+"?="+s1.equals(s3));
+
+        
+    }
+}
